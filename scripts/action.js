29646ed9b66bcde308cb91d367eb5d3b8a6785b9
Jerliyah <jerliyahcraig@gmail.com>
2017-01-21 15:49:55 -0600

    quote api added. TODO: formatting

---
+++ scripts/action.js
@@ -0,0 +1,33 @@
+
+    function errorMessage() {
+        $("main").html("<h1>Oops!</h1> <h3>There was an error retrieving the quote</h3>");
+    }
+
+
+    function formatting(response) {
+
+        var quote = response.quoteText;
+        $("main").html('<h1>' + quote + '</h1> <br> <h5> -- ' + response.quoteAuthor);
+
+    }
+
+    // Learned Something: For ajax call, don't place parenthesis with function name
+    function randomQuote() {
+        $.ajax({
+          type: "GET",
+          url: "http://api.forismatic.com/api/1.0/?",
+          dataType: "jsonp",
+          data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+          success: formatting,
+          error: errorMessage
+      });
+    }
+
+
+//------------------------------------------------------------------------------
+
+// A quote shows up when page loads
+    randomQuote();
+
+// A new quote is generated everytime the new quote button is clicked
+    $("#new-quote-btn").click( randomQuote );
