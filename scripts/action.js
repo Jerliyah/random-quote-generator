bf79a07a215b62b9df41957b22ba30f6e92c2cf3
Jerliyah <jerliyahcraig@gmail.com>
2017-01-23 21:35:09 -0600

    ending quote mark at proper height, and both quotes marks  have some space away from text

--- scripts/action.js
+++ scripts/action.js
@@ -6,9 +6,11 @@
 
     function formatting(response) {
 
-        var quote = response.quoteText;
-        $("main").html('<h1>' + quote + '</h1> <br> <h5> -- ' + response.quoteAuthor);
+        var leftQuoteMark = '<i class="fa fa-quote-left fa-2x" aria-hidden="true"></i>';
+        var rightQuoteMark = '<i class="fa fa-quote-right fa-2x" aria-hidden="true"></i>';
 
+        var quote = response.quoteText;
+        $("main").html('<h1>' + leftQuoteMark + ' ' + quote + rightQuoteMark + ' </h1> <br> <h4> -- ' + response.quoteAuthor + '<h4>');
     }
 
     // Learned Something: For ajax call, don't place parenthesis with function name
@@ -24,10 +26,17 @@
     }
 
 
+    function loading() {
+        $("main").html('<i class="fa fa-cog fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span>');
+    }
+
 //------------------------------------------------------------------------------
 
 // A quote shows up when page loads
     randomQuote();
 
 // A new quote is generated everytime the new quote button is clicked
-    $("#new-quote-btn").click( randomQuote );
+    $("#new-quote-btn").click( function() {
+        loading();
+        randomQuote();
+    } );
